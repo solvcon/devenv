803756583aa3e23e31b798d0ba6712b8fec27d0d
Taihsiang Ho (tai271828) <tai271828@python.tw>
2021-09-29 23:24:22 +0200

    feat(solvcon): integrate cython built by devenv

--- applications/solvcon/conda.sh
+++ applications/solvcon/conda.sh
@@ -2,7 +2,7 @@
 conda install -y \
   python=3.6 \
   cmake setuptools pip sphinx ipython jupyter \
-  cython numpy hdf4 netcdf4 nose pytest paramiko boto graphviz
+  numpy hdf4 netcdf4 nose pytest paramiko boto graphviz
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
 conda install -y -c https://conda.anaconda.org/yungyuc scotch
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
