dc68c0c12938bc0f8546518d4215f7a51f54170c
Taihsiang Ho (tai271828) <e@tai271828.me>
2021-10-24 03:31:26 +0200

    feat(solvcon): migrate cmake from conda to devenv

--- applications/solvcon/conda.sh
+++ applications/solvcon/conda.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 conda install -y \
   python=3.8 \
-  cmake \
   numpy hdf4 netcdf4 nose boto paramiko
-lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
-conda install -y -c https://conda.anaconda.org/yungyuc scotch
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
