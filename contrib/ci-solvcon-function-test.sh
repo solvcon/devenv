3250ed0613b30586d674738bf9f135e36de5f0ba
Taihsiang Ho (tai271828) <e@tai271828.me>
2022-02-05 17:13:05 +0100

    ci(SOLVCON): enhance the build scripts
    Enhance the scripts so it will be more fool-proof. For example:
    1. exit when DEVENV_WORKSPACE is not set and give a hint.
    2. fix typo of the shebang.
    3. fix the mixed tabs and spaces

--- contrib/ci-solvcon-function-test.sh
+++ contrib/ci-solvcon-function-test.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -eo pipefail
+if [ -z "${DEVENV_WORKSPACE}" ]; then
+  echo DEVENV_WORKSPACE is not available. Abort!
+  exit 1
+fi
+
 source ${DEVENV_WORKSPACE}/scripts/init
 
 devenv use prime
