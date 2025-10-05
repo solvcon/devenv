c2ab80d568fa2c94d4f1a69ccf6a973282daa2f1
Terry Chan <terrychan999@gmail.com>
2025-10-05 22:54:31 +0800

    fix(solvcon): update Python to 3.8.20 to fix download failure

--- contrib/ci-setup-apt.sh
+++ contrib/ci-setup-apt.sh
@@ -4,3 +4,7 @@ sudo apt-get -qqy install \
         fakeroot debhelper locales \
         libffi-dev \
         liblapack3 liblapack-dev
+
+# for sphinx's latex engine
+sudo apt-get install -qqy texlive-latex-base texlive-latex-recommended latexmk
+sudo apt-get install -qqy ghostscript imagemagick
